bf12b0e842cbc6cf8f184a5b0cdad2f3b37b881b
Artem Martirosyan <syanches@gmail.com>
2019-03-07 23:53:12 +0300

    made my "Remember Your Emojis"  Lab

--- EmojiDictionary/Emoji.swift
+++ EmojiDictionary/Emoji.swift
@@ -9,13 +9,16 @@
 import Foundation
 
 
-class Emoji {
+class Emoji: Codable {
     var symbol: String
     var name: String
     var description: String
     var usage: String
-    //var emojis: [Emoji]
     
+    static var ArchiveURL: URL{
+        let documentDirectory = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask).first!
+        return documentDirectory.appendingPathComponent("emoji").appendingPathExtension("plist")
+    }
     
     init(symbol: String, name: String, description: String, usage:String){
         self.symbol = symbol
@@ -24,7 +27,45 @@ class Emoji {
         self.usage = usage
     }
 
+    static func saveToFile(emojis: [Emoji]){
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedEmojis = try? propertyListEncoder.encode(emojis)
+        try? encodedEmojis?.write(to: ArchiveURL)
+    }
     
+    static func loadFromFile() -> [Emoji]?{
+        let propertyListDecoder = PropertyListDecoder()
+        
+        if let retrieveEmojiData = try? Data(contentsOf: ArchiveURL),
+            let decodedEmojis = try? propertyListDecoder.decode(Array<Emoji>.self, from: retrieveEmojiData) {
+            return decodedEmojis
+        }
+        return nil
+    }
+    
+    static func loadSampleEmojis() -> [Emoji] {
+     var emojis: [Emoji] = [
+     Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
+     Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
+     Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
+     Emoji(symbol: "👮", name: "Police Officer", description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"),
+     Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"),
+     Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
+     Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
+     Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
+     Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
+     Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"),
+     Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"),
+     Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
+     Emoji(symbol: "🏁", name: "Chesdackered Flag", description: "A black-and-white checkered flag.", usage: "completion"),
+     Emoji(symbol: "🌑", name: "bnBroken Heart", description: "A red, broken heart.", usage: "extreme sadness"),
+     Emoji(symbol: "🌈", name: "Snbhjore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
+     Emoji(symbol: "🍠", name: "mnbCheckered Flag", description: "A black-and-white checkered flag.", usage: "completion")
+     ]
+        
+        return emojis
+    }
+ 
 }
 
 
--- EmojiDictionary/EmojiTableTableViewController.swift
+++ EmojiDictionary/EmojiTableTableViewController.swift
@@ -9,31 +9,11 @@
 import UIKit
 
 class EmojiTableTableViewController: UITableViewController {
-
-
-    
-    
-    
-    var emojis: [Emoji] = [
-        Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
-        Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
-        Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
-        Emoji(symbol: "👮", name: "Police Officer", description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"),
-        Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"),
-        Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
-        Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
-        Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
-        Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
-        Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"),
-        Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"),
-        Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
-        Emoji(symbol: "🏁", name: "Chesdackered Flag", description: "A black-and-white checkered flag.", usage: "completion"),
-        Emoji(symbol: "🌑", name: "bnBroken Heart", description: "A red, broken heart.", usage: "extreme sadness"),
-        Emoji(symbol: "🌈", name: "Snbhjore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
-        Emoji(symbol: "🍠", name: "mnbCheckered Flag", description: "A black-and-white checkered flag.", usage: "completion")
-    ]
-    
-    
+    var emojis: [Emoji] = [] {
+        didSet {
+            Emoji.saveToFile(emojis: emojis)
+        }
+    }
     
     @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
         let tableViewEditingMode = tableView.isEditing
@@ -44,6 +24,12 @@ class EmojiTableTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = editButtonItem
+        
+        if let emojisFromFile = Emoji.loadFromFile() {
+            emojis = emojisFromFile
+        } else {
+            emojis = Emoji.loadSampleEmojis()
+        }
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
